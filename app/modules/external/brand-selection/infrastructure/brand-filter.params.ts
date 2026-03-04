2d23b53de16eb150dcf08b36b2d6d05828ef25c9
adjie <adjiesidja20@gmail.com>
2026-03-04 21:20:12 +0700

    change : from brand-selection to brand

--- app/modules/external/brand-selection/infrastructure/brand-filter.params.ts
+++
@@ -1,7 +0,0 @@
-import type { PaginationParams } from "~/core/api/pagination.type";
-
-export interface BrandFilterParams extends Partial<PaginationParams> {
-  order_by?: string;
-  category?: string;
-  location?: string;
-}
--- app/modules/internal/brand-selection/presentation/brand-selection.page.tsx
+++
@@ -1,4 +0,0 @@
-/** Internal — Brand Selection (Pilih Brand) */
-export default function BrandSelectionPage() {
-  return <div>Pilih Brand</div>;
-}
